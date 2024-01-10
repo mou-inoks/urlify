94dd19bddcb62e03e7c7b82b8d5b0e587a9d0d36
mou-inoks <mou@inokscapital.com>
2024-01-10 22:12:42 +0100

    first commit

---
+++ URLify/Program.cs
@@ -0,0 +1,14 @@
+﻿string URLify(string word)
+{
+    var newWord = "";
+    foreach (var i in word)
+    {
+        if (i == ' ')
+            newWord += "%20";
+        else newWord += i;
+    }
+
+    return newWord;
+}
+
+Console.WriteLine(URLify("This is my world"));
